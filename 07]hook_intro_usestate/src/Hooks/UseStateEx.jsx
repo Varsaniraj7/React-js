21fc2b36b08257ea957c31cdc05018791a268521
Varsaniraj7 <Varsaniraj7@gmail.com>
2024-10-09 16:18:53 +0530

    commited

---
+++ 07]hook_intro_usestate/src/Hooks/UseStateEx.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+export default function UseStateEx() {
+    const [count,setCount] = useState(0);
+    
+    const increment = ()=>{
+        setCount(count+1)
+    }
+    
+    const reset = ()=>{
+        setCount(0)
+    }
+
+    const decrement = ()=>{
+        setCount(count-1)
+    }
+
+    return (
+        <div>
+            <h1>Hooks UseState</h1>
+            <h1>{count}</h1>
+            <button onClick={decrement} >-</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment} >+</button>
+        </div>
+    )
+}
